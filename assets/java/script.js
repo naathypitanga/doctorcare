81b5038f85f53ae1167482a50de10d43ff92471b
Lucas Pitanga <yukimura6523@gmail.com>
2022-05-08 09:33:36 -0300

    first commit

---
+++ assets/java/script.js
@@ -0,0 +1,69 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    const sectionEndAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetline = sectionEndAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline 
+
+    const sectionId = section.getAttribute('id')
+
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+}
+
+function showNavOnScroll(){
+    if(scrollY > 0){
+        navigation.classList.add('scroll')
+        }else {
+            navigation.classList.remove('scroll')
+        }
+}
+
+function showBackToTopButtonOnScroll(){
+    if(scrollY > 500){
+        backToTopButton.classList.add('show')
+        }else {
+            backToTopButton.classList.remove('show')
+        }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal('#home, #home img, #home .statsclass, #services, #services header, #services .card, #about, #about header, #about .content');
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projeto DoctorCare (NLW Return)
+
+
+
+Projeto executado durante uma semana na plataforma RocketSeat (NLW Return Origin), onde foi criado um site responsivo para a Assistência Médica.
